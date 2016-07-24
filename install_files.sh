e2f823ac3ceff7ec04a0a901f73c87a3ba6d46db
Will Ellwood <wme@localhost.localdomain>
2016-07-24 16:34:03 +0100

    Added basic install script
    The script install_files.sh has been added to install Cask, configuration.org, and tmux.conf files as symbolic links.
    
    Emacs currently picks up on the fact that configuration.org is a symbolic link to a git controlled directory and prompts me on startup if this is okay. I will need to find a workaround to surpress this message as it should
    generally be treated as okay. The script also has no intellegence so will fail if presented with preexisting files.

---
+++ install_files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/emacs.d/configuration.org ~/.emacs.d/configuration.org
+ln -s ~/dotfiles/emacs.d/Cask ~/.emacs.d/Cask
