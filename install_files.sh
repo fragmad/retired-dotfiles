5e0d05dd20ac59c9e73e4f38b17d6262e2b8faad
Will Ellwood <william.ellwood@heg.com>
2016-09-09 17:14:48 +0100

    Lots of changes! Terrible commit message.

--- install_files.sh
+++ install_files.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+ls -s ~/dotfiles/emacs.d/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/emacs.d/configuration.org ~/.emacs.d/configuration.org
 ln -s ~/dotfiles/emacs.d/Cask ~/.emacs.d/Cask
