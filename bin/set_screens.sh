46a458f11325d7852cb510bc1317bb353244a6f2
William Ellwood <fragmad@gmail.com>
2018-11-14 17:19:29 +0000

    Added screen management things. Experimental and will annoy me.

---
+++ bin/set_screens.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+HOSTNAME=`hostname`
+work_monitor_one=`xrandr |grep DP-1-1 -c`
+
+if [ "$HOSTNAME" = "well-mobile" ] && [ $work_monitor_one -eq 1 ] 
+then 
+  exec ~/.screenlayout/work-screens.sh
+fi
+
