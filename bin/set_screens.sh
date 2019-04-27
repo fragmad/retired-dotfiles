98c4bd0d2c0bf4f002cb3404276fdd7b3536e38e
Will Ellwood <fragmad@gmail.com>
2019-04-27 18:20:37 +0100

    More quality of life changes
    The monitor selection one is very important

--- bin/set_screens.sh
+++ bin/set_screens.sh
@@ -2,10 +2,14 @@
 
 HOSTNAME=`hostname`
 work_monitor_one=`xrandr |grep DP-1-1 -c`
+home_monitor=`xrandr | grep ^DP-1 -c`
 
-if [ "$HOSTNAME" = "well-mobile" ] && [ $work_monitor_one -eq 1 ] 
-then 
-  exec ~/.screenlayout/work-screens.sh
+if [ "$HOSTNAME" = "well-mobile" ] && [ $work_monitor_one -eq 1 ]
+then
+    exec ~/.screenlayout/work-screens.sh
+elif [ "$HOSTNAME" = "nostromo" ] && [ $home_monitor -eq 1 ]
+then
+    exec ~/.screenlayout/home.sh
 else
-  exec ~/.screenlayout/laptop_screen.sh
+    exec ~/.screenlayout/laptop_screen.sh
 fi
